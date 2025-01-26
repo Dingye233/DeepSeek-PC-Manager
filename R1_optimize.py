e98a77e5f47fdd7931f20d02f1055513162913e5
Dingye233 <1792491376@qq.com>
2025-01-26 14:16:24 +0800

    Auto commit by PC-Agent

--- R1_optimize.py
+++ R1_optimize.py
@@ -7,7 +7,7 @@ load_dotenv()
 def r1_optimizer(message:str)->str:
     client = OpenAI(api_key=os.environ.get("api_key"), base_url="https://api.deepseek.com/beta")
 
-    messages=[{"role": "user", "content": "判断用户是否有代码,命令,文本编辑的需求，如果没有这方面的需求直接输出一个空的字符串: "+message},{"role": "assistant", "content": "```python\n", "prefix": True}]
+    messages=[{"role": "user", "content":message},{"role": "assistant", "content": "```python\n", "prefix": True}]
     response_r1 = client.chat.completions.create(
         model="deepseek-reasoner",
         messages=messages,
@@ -15,4 +15,16 @@ def r1_optimizer(message:str)->str:
     )
     reasoning_content = response_r1.choices[0].message.reasoning_content
     content = response_r1.choices[0].message.content
+    return str(content)
+def collect_user_information(message:str)->dict:
+    client = OpenAI(api_key=os.environ.get("api_key"), base_url="https://api.deepseek.com/beta")
+
+    messages = [{"role": "user", "content": message}]
+    response_r2 = client.chat.completions.create(
+        model="deepseek-reasoner",
+        messages=messages,
+
+    )
+    reasoning_content = response_r2.choices[0].message.reasoning_content
+    content = response_r2.choices[0].message.content
     return str(content)
